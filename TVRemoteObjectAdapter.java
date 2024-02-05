c76ce0cbec6569b652ddaef44daf75beebd0cc0f
Yongxiang Cai <79479480+XiaoSanchez@users.noreply.github.com>
2024-02-04 22:14:45 -0500

    Add files via upload

--- TVRemoteObjectAdapter.java
+++ TVRemoteObjectAdapter.java
@@ -4,15 +4,14 @@ package assignment01;
 // It is really helpful to notice that the methods up() and down() in TV prevent 
 // the TV channel from going above its maximum or below 2.
 
-
 public class TVRemoteObjectAdapter implements TVRemote {
 	private TV tv;
 	// the remote has to keep its own copy of the TVs channel
 	private int tvChannel = 2;
-	
+
 	public TVRemoteObjectAdapter(TV tvIn) {
 		tv = tvIn;
-		// put a loop in to make sure the TV at channel 2 
+		// put a loop in to make sure the TV at channel 2
 		// by calling the the down() method the maximum number of times
 		// (use tv.MAX_CHANNELS)
 		for(int i = 0; i < tv.MAX_CHANNELS; i++) {
@@ -20,9 +19,9 @@ public class TVRemoteObjectAdapter implements TVRemote {
 		}
 		tvChannel = 2;
 	}
-	
+
 	// 4 other methods defined in the interface
-	
+
 	public void setChannel(int newChannel) {
 		// NOTE if parameter is out of limits, the loops will be infinite
 		newChannel = Math.min(newChannel, tv.MAX_CHANNELS);
@@ -30,16 +29,24 @@ public class TVRemoteObjectAdapter implements TVRemote {
 		// write loops to move the tv to the correct channel.
 		// update tvChannel to be newChannel
 		while(tvChannel < newChannel) {
-			tv.up();
-			tvChannel++;
+			channelUp();
 		}
 		while(tvChannel > newChannel) {
-			tv.down();
-			tvChannel--; 
+			channelDown();
 		}
-	} 
+	}
+
 	public void showChannel() {
 		System.out.print("Remote Channel is " + tvChannel + ". ");
 	}
-}
 
+	public void channelUp() {
+		tv.up();
+		tvChannel++;
+	}
+
+	public void channelDown() {
+		tv.down();
+		tvChannel--;
+	}
+}
